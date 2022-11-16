e2930fe276de04c66890fb27fa3b46ac01a0ee11
Tony Chen <tony.chen@aboutscript.net>
2022-11-16 18:04:11 +0800

    Update single_tax field type to Many2many for purchase and sale single tax module. Add related to taxes_id field for purchase and sale single tax module.

--- sale_single_tax/models/sale.py
+++ sale_single_tax/models/sale.py
@@ -17,35 +17,16 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    single_tax = fields.Many2one('account.tax', string='Tax', domain='[("type_tax_use", "=", "sale")]', default=lambda self: self.env.company.account_sale_tax_id)
+    single_tax = fields.Many2many('account.tax', string='Tax', domain='[("type_tax_use", "=", "sale")]')
 
-    @api.onchange('single_tax', 'order_line', 'order_line.product_id', 'order_line.tax_id')
-    def single_tax_change(self):
-        """
-        When those field change will change product taxes in order line field
 
-        :return: None
-        """
+class SaleOrderLine(models.Model):
+    """
+    Add related to tax_id filed
 
-        for order in self:
-            order.order_line.tax_id = order.single_tax
+    [sale.order.line]
+    """
 
-    def action_confirm(self):
-        """
-        When user confirm the order. Product tax will become same as single tax
+    _inherit = 'sale.order.line'
 
-        :return: None
-        """
-
-        self.single_tax_change()
-        return super(SaleOrder, self).action_confirm()
-
-    def action_quotation_send(self):
-        """
-        When user send the quotation of order. Product tax will become same as single tax
-
-        :return: Action
-        """
-
-        self.single_tax_change()
-        return super(SaleOrder, self).action_quotation_send()
+    tax_id = fields.Many2many(related='order_id.single_tax')
